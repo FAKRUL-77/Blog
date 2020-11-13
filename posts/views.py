86632162382cf6a0fdb6ed07d790a5e13ee5c1f3
FAKRUL-77 <fakrul15-1316@diu.edu.bd>
2020-11-13 18:25:50 +0600

    [ADD] ADDED REFERENCE FOR GETTING POST FROM JSON JSONPLACEHOLDER Create a view for getting post and making pagination     - Get posts from jsonplaceholder using requests     - Make pagination with four post at each page     - Edited blog-listing.html to show posts Resolves: #2

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,22 @@
+import requests
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 
 # POSTS VIEW ENDPOINT
 def posts(request):
-    return render(request, 'blog-listing.html')
+    req = requests.get("https://jsonplaceholder.typicode.com/posts")
+    objects = req.json()
+    paginator = Paginator(objects, 4)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'blog-listing.html', {'page_obj': page_obj})
 
 
 # POST DETAILS VIEW ENDPOINT
