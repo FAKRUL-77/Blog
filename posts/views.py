7182386b45f16be31f47a8b516843650fdad94fe
FAKRUL-77 <fakrul15-1316@diu.edu.bd>
2020-11-13 18:45:30 +0600

    [ADD] ADDED REFERENCE FOR GETTING SPECIFIC POST AND IT'S COMMENTS Create a view for getting specific post and it's comment and show them     - Get posts from jsonplaceholder.typicoder.com/posts/1 url  using requests     - Get posts comment from jsonplaceholder.typicoder.com/posts/1/comments url  using requests     - Convert collected data into json format     - Edited blog-post.html to show specific post and it's comments. Resolves: #2

--- posts/views.py
+++ posts/views.py
@@ -20,5 +20,20 @@ def posts(request):
 
 
 # POST DETAILS VIEW ENDPOINT
-def post_details(request):
-    return render(request, 'blog-post.html')
+def post_details(request, post_id):
+
+    post_link = "https://jsonplaceholder.typicode.com/posts/" + str(post_id)
+    post_comment_link = post_link + '/comments'
+
+    post_request = requests.get(post_link)
+    comment_request = requests.get(post_comment_link)
+
+    post_object = post_request.json()
+    post_comments_object = comment_request.json()
+
+    context = {
+        'post_obj': post_object,
+        'comments_obj': post_comments_object,
+    }
+
+    return render(request, 'blog-post.html', context)
