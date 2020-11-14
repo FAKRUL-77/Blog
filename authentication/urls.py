7d171eba3a4b04549205ffe9c110886e6a19b2f6
FAKRUL-77 <fakrul15-1316@diu.edu.bd>
2020-11-15 00:26:01 +0600

    [EDIT] MODIFY USER LOGIN VIEW     - Update user login view (function based view to class based view)
    Resolces: #7

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from authentication.views import userLoginView, UserRegisterView, UserLogoutView, UserLoginView
+from authentication.views import UserRegisterView, UserLogoutView, UserLoginView
 
 urlpatterns = [
-    # path('login/', userLoginView, name='login'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('register/', UserRegisterView.as_view(), name='register'),
